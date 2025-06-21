ea9d339df3985d1a739280aefcee64e2c9f4ad2d
ahmed mhmoud <ahmed.said03@eng-st.cu.edu.eg>
2025-06-21 11:35:15 +0000

    SQL - Students and Examinations

---
+++ Leetcode/SQL/Students and Examinations.sql
@@ -0,0 +1,11 @@
+SELECT 
+    s.student_id,
+    s.student_name,
+    sb.subject_name,
+    COUNT(e.student_id) as attended_exams
+FROM students s
+CROSS JOIN subjects sb
+LEFT JOIN examinations e ON s.student_id = e.student_id 
+                        AND sb.subject_name = e.subject_name
+GROUP BY s.student_id, s.student_name, sb.subject_name
+ORDER BY s.student_id, sb.subject_name;
